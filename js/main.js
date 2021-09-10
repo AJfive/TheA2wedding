77d8083a66310e4c3a8f78b25c73faf084e15c1d
AJfive <89221417+AJfive@users.noreply.github.com>
2021-09-11 01:04:03 +0530

    New Index file uploaded

--- js/main.js
+++ js/main.js
@@ -239,6 +239,20 @@
 	});
 
 
+	/* Blog slider */
+	$('.blog-slider').slick({
+		slidesToShow: 2,
+		prevArrow: '<span class="span-arrow slick-prev"><</span>',
+		nextArrow: '<span class="span-arrow slick-next">></span>',
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+		]
+	});
 
 
 var counta = 0;
